e61bfdf51fe5954bb5de37f56ab61332704d68a2
“Akhilkumar1f4” <akhilkumar1f4@gmail.com>
2023-05-18 22:48:30 +0530

    UC3-lineCompare-lineCompare

---
+++ LineComparisonComputation/lineCompare/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+public class Point
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+}
+
+public class LineSegment : IComparable<LineSegment>
+{
+    public Point StartPoint { get; set; }
+    public Point EndPoint { get; set; }
+
+    public int CompareTo(LineSegment other)
+    {
+        double thisLength = CalculateLength();
+        double otherLength = other.CalculateLength();
+
+        if (thisLength < otherLength)
+        {
+            return -1; // This line is shorter
+        }
+        else if (thisLength > otherLength)
+        {
+            return 1; // This line is longer
+        }
+        else
+        {
+            return 0; // Both lines have equal length
+        }
+    }
+
+    private double CalculateLength()
+    {
+        int dx = EndPoint.X - StartPoint.X;
+        int dy = EndPoint.Y - StartPoint.Y;
+        return Math.Sqrt(dx * dx + dy * dy);
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Create line segments
+        LineSegment line1 = new LineSegment
+        {
+            StartPoint = new Point { X = 0, Y = 0 },
+            EndPoint = new Point { X = 3, Y = 4 }
+        };
+
+        LineSegment line2 = new LineSegment
+        {
+            StartPoint = new Point { X = 0, Y = 0 },
+            EndPoint = new Point { X = 6, Y = 8 }
+        };
+
+        // Compare the line segments
+        int comparisonResult = line1.CompareTo(line2);
+
+        if (comparisonResult < 0)
+        {
+            Console.WriteLine("Line 1 is shorter than Line 2");
+        }
+        else if (comparisonResult > 0)
+        {
+            Console.WriteLine("Line 1 is longer than Line 2");
+        }
+        else
+        {
+            Console.WriteLine("Line 1 is equal in length to Line 2");
+        }
+    }
+}
