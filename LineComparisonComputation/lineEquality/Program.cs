981d02a7d20353fc18e41ca4ff9093acd9add499
“Akhilkumar1f4” <akhilkumar1f4@gmail.com>
2023-05-18 22:30:38 +0530

    UC2:lineEquality-lineEquality-UC

---
+++ LineComparisonComputation/lineEquality/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+public class Point
+{
+    public int X { get; }
+    public int Y { get; }
+
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj == null || GetType() != obj.GetType())
+            return false;
+
+        Point other = (Point)obj;
+        return X == other.X && Y == other.Y;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(X, Y);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Point startPoint1 = new Point(0, 0);
+        Point endPoint1 = new Point(1, 1);
+
+        Point startPoint2 = new Point(0, 0);
+        Point endPoint2 = new Point(1, 1);
+
+        if (startPoint1.Equals(startPoint2) && endPoint1.Equals(endPoint2))
+        {
+            Console.WriteLine("The lines are equal.");
+        }
+        else
+        {
+            Console.WriteLine("The lines are not equal.");
+        }
+    }
+}
